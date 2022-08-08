5edf421711012ed0fdf65f747d50351f18fe62b4
Estefanny Leyva <fanniluley88@gmail.com>
2022-08-07 23:20:02 -0500

    add practices and homework

---
+++ algoritmos/tarea/js/main.js
@@ -0,0 +1,16 @@
+let edad = 22;
+
+if (edad >= 3 && edad < 6 ) {
+    console.log("actualmente deberias estar cursando preescolar")
+}else if (edad >= 6 && edad <= 11 ) {
+    console.log("actualmente deberias estar cursando primaria")
+}else if (edad >= 12 && edad <= 14 ) {
+    console.log("actualmente deberias estar cursando secundaria")
+}else if (edad >= 15 && edad <= 17 ) {
+    console.log("actualmente deberias estar cursando preparatoria")
+}else if (edad >= 18 && edad <= 25 ) {
+    console.log("actualmente deberias estar cursando universidad")
+}else {
+    console.log("ya no pertenece a un grado de estudios")
+}
+
