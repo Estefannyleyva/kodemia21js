272c78f6953608a07556fd6ace68f9928165e16b
Estefanny Leyva <fanniluley88@gmail.com>
2022-08-08 00:29:57 -0500

    add typeof and array

--- algoritmos/tarea/js/main.js
+++ algoritmos/tarea/js/main.js
@@ -1,42 +1,55 @@
 let edad = 22;
+const gradoDeEstudios = [
+    "preescolar",
+    "primaria",
+    "secundaria",
+    "preparatoria",
+    "universidad"
+];
 
 if (edad >= 3 && edad < 6 ) {
-    console.log("actualmente deberias estar cursando preescolar")
+    console.log("actualmente deberias estar cursando" + " " + gradoDeEstudios[0]);
 }else if (edad >= 6 && edad <= 11 ) {
-    console.log("actualmente deberias estar cursando primaria")
+    console.log("actualmente deberias estar cursando" + " " + gradoDeEstudios[1]);
 }else if (edad >= 12 && edad <= 14 ) {
-    console.log("actualmente deberias estar cursando secundaria")
+    console.log("actualmente deberias estar cursando" + " " + gradoDeEstudios[2]);
 }else if (edad >= 15 && edad <= 17 ) {
-    console.log("actualmente deberias estar cursando preparatoria")
+    console.log("actualmente deberias estar cursando" + " " + gradoDeEstudios[3]);
 }else if (edad >= 18 && edad <= 25 ) {
-    console.log("actualmente deberias estar cursando universidad")
+    console.log("actualmente deberias estar cursando" + " " + gradoDeEstudios[4]);
+}else if (typeof edad !== 'number') {
+    console.log("no estas ingresando un numero");
 }else {
-    console.log("ya no pertenece a un grado de estudios")
+    console.log("ya no pertenece a un grado de estudios");
 }
 
 switch (true) {
     case edad >= 3 && edad < 6:
-        console.log("actualmente deberias estar cursando preescolar");
+        console.log("actualmente deberias estar cursando" + " " + gradoDeEstudios[0]);
         
         break;
     case edad >= 6 && edad <= 11:
-        console.log("actualmente deberias estar cursando primaria");
+        console.log("actualmente deberias estar cursando" + " " + gradoDeEstudios[1]);
         
         break;
     case edad >= 12 && edad <= 14:
-        console.log("actualmente deberias estar cursando secundaria");
+        console.log("actualmente deberias estar cursando" + " " + gradoDeEstudios[2]);
         
         break;
     case edad >= 15 && edad <= 17:
-        console.log("actualmente deberias estar cursando preparatoria");
+        console.log("actualmente deberias estar cursando" + " " + gradoDeEstudios[3]);
 
         break;
     case edad >= 18 && edad <= 25:
-        console.log("actualmente deberias estar cursando universidad");
+        console.log("actualmente deberias estar cursando" + " " + gradoDeEstudios[4]);
 
         break;
     case edad >= 18 && edad <= 25:
-        console.log("actualmente deberias estar cursando universidad");
+        console.log("actualmente deberias estar cursando" + " " + gradoDeEstudios[5]);
+    
+        break;
+    case typeof edad !== 'number':
+        console.log("no estas ingresando un numero");
     
         break;
         
