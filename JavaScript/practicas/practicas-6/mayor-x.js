8b4c5fe8c8f98c56a60033041b2ed1f0c89dcf3a
Estefanny Leyva <fanniluley88@gmail.com>
2022-08-15 15:05:15 -0500

    numeros mayores a x exercise

---
+++ JavaScript/practicas/practicas-6/mayor-x.js
@@ -0,0 +1,15 @@
+let arregloNumeros = [1, 2, 3, 4, 5, 6, 7, 8];
+let mayor = 4;
+let total = 0;
+let numerosMayores= function(numeros, mayor) {
+    
+    for (let i = 0; i < numeros.length; i++) {
+        
+       if (numeros[i] > mayor) {
+          total++;
+            
+        }
+    }
+    console.log(total);
+}
+numerosMayores(arregloNumeros, mayor);
