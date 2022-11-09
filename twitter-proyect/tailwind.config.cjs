68271dbffdfbcd8431491246229de4b07e2a7476
Estefanny Leyva <fanniluley88@gmail.com>
2022-11-09 10:36:48 -0600

    add twitter project

---
+++ twitter-proyect/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "twitter-gray": "#14181c",
+      },
+    },
+  },
+  plugins: [],
+};
